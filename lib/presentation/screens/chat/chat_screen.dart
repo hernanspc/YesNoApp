8d4cb3d96ed112847bb3b330eb61d174ce1f1f16
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-08-15 20:24:01 -0500

    refactor

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,26 +1,51 @@
-import 'dart:math';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:yes_no_app/presentation/entities/message.dart';
 import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+import 'package:yes_no_app/utils/utils.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  File? image;
+
+  void selectImage() async {
+    // print('s');
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      print('Ruta de la imagen seleccionada: ${pickedImage.path}');
+    }
+
+    // image = await pickImage(context);
+    // setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(4.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-              'https://esp.unbilgi.com/wp-content/uploads/2021/05/Lana-Rhoades-Facts.jpg',
-              // 'https://techstory.in/wp-content/uploads/2022/02/lana-rhoades-fb-1.jpg',
+        leading: GestureDetector(
+          onTap: () => selectImage(),
+          child: Padding(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://esp.unbilgi.com/wp-content/uploads/2021/05/Lana-Rhoades-Facts.jpg',
+                // 'https://techstory.in/wp-content/uploads/2022/02/lana-rhoades-fb-1.jpg',
+              ),
             ),
           ),
         ),
